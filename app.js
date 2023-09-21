8a6ac90bee9399acad06ad55f01d99143f4b7518
mutant138 <udhayasurya138@gmail.com>
2023-09-21 16:59:57 +0530

    Redirecting req

--- app.js
+++ app.js
@@ -1,31 +1,24 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
     //console.log(req.url,req.method,req.headers)
     //process.exit();
     const url=req.url;
-    if(url === '/home'){
-    res.setHeader('Content-Type', 'text/html');
+    const method=req.method;
+    if(url === '/'){
+    //res.setHeader('Content-Type', 'text/html');
     res.write('<html>')
     res.write('<head><title>My first Page</title></head>')
-    res.write('<body><h1>Welcome Home</h1></body>')
+    res.write('<body><h1>Welcome Home</h1><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
     res.write('</html>')
     return res.end();
-    }else if(url === '/about'){
-     res.setHeader('Content-Type', 'text/html');
-    res.write('<html>')
-    res.write('<head><title>My first Page</title></head>')
-    res.write('<body><h1>Welcome to About Us page</h1></body>')
-    res.write('</html>')
-    return res.end();
-    }else if(url === '/node'){
-        res.setHeader('Content-Type', 'text/html');
-       res.write('<html>')
-       res.write('<head><title>My first Page</title></head>')
-       res.write('<body><h1>Welcome to my Node Js project</h1></body>')
-       res.write('</html>')
-       return res.end();
-       }
-       
+    }
+    if(url === "/message" && method==='POST') {
+       fs.writeFileSync('message.txt','Dummy')
+       res.statusCode=302;
+       res.setHeader('Location','/');
+       return res.end()
+    }
 });
 server.listen(3000);
