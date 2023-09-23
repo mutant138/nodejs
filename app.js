dc0773391a0b1e40b02854f2da60058a8a5bb2d9
mutant138 <udhayasurya138@gmail.com>
2023-09-23 16:33:06 +0530

    ExpressJS

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const http = require('http');
-const routes = require('./routes');
+const express = require('express');
 
-const { log } = require('console');
-console.log(routes.someText)
-const server = http.createServer(routes.handler);
+const app= express();
 
+app.use("/add-product",(req,res,next)=>{
+    console.log('In another middleware');
+    res.send('<h1>The add product</h1>')
+})
+app.use('/',(req,res,next)=>{
+    console.log('In another middleware');
+    res.send('<h1>Hello from Express</h1>')
+})
 
-server.listen(3000);
+//const { log } = require('console');
+//console.log(routes.someText)
+
+app.listen(3000);
