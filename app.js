fc22dd65c18027f5c73b33bbd68b0b1c582ceaa1
mutant138 <udhayasurya138@gmail.com>
2023-09-23 21:03:16 +0530

    Routes and filtering

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser')
-const app= express();
-app.use(bodyParser.urlencoded())
-app.use("/add-product",(req,res,next)=>{
-    //console.log('In another middleware');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add product</button></form>')
-})
-app.post('/product',(req,res,next)=>{
-   console.log(req.body)
-   res.redirect('/')
-})
-app.use('/',(req,res,next)=>{
-    //console.log('In another middleware');
-    res.send('<h2>Hell from Express JS<h2>')
-})
 
-//const { log } = require('console');
-//console.log(routes.someText)
+const app= express();
+
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
+app.use(bodyParser.urlencoded())
+
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes)
+
+app.use((req, res, next) => {
+ res.status(404).send('<h1>Page Not Found</h1>');
+});
+
 
 
 app.listen(3000);
